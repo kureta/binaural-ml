f1f17982c165233c32cb005fa092f3502da9dbf6
kureta <skureta@gmail.com>
2025-04-29 16:38:55 +0300

    Cleaned up binaural doppler code

--- notebooks/exploration.py
+++ notebooks/exploration.py
@@ -11,34 +11,34 @@ def _():
     import soundfile
     import numpy as np
     from matplotlib import pyplot as plt
-    from scipy.signal import butter, lfilter
 
 
     import math
-    return np, plt, soundfile
+    import marimo as mo
+    return mo, np, soundfile
 
 
 @app.cell
-def _(np, soundfile):
-    audio, sample_rate = soundfile.read("horn.wav")
-    mono = audio  # [:, 0]
-    mono = mono / np.max(np.abs(mono))
+def _(soundfile):
+    audio, sample_rate = soundfile.read("data/horn.wav")
+    # Get only the first channel if the audio is multichannel
+    if len(audio.shape) > 1:
+        audio = audio[:, 0]
+
     sample_rate
-    return mono, sample_rate
+    return audio, sample_rate
 
 
 @app.cell
-def _(mono, np, sample_rate):
+def _():
     c = 343  # m/s
     head_radius = 0.09
-    samples_per_meter = sample_rate / c  # sample density in space
-    distance_between_samples = c / sample_rate
-    d = np.arange(0, len(mono)) * distance_between_samples
     return c, head_radius
 
 
 @app.cell
-def _(head_radius, np):
+def _(audio, c, np, sample_rate):
+    # Define some orbits for the source
     def fn_elliptic_orbit(t, period=20, rx=50, ry=2):
         f = 1 / period
         return rx * np.cos(f * 2 * np.pi * t), ry * np.sin(f * 2 * np.pi * t)
@@ -48,75 +48,69 @@ def _(head_radius, np):
         return v * t + x0, y_offset
 
 
+    # Cartesian to polar coordinates
     def to_polar(position):
         return np.hypot(*position), np.arctan2(*position[::-1])
 
 
-    def to_polar_stereo(position):
-        x, y = position
-        x_left = x + head_radius
-        x_right = x - head_radius
-        left = to_polar((x_left, y))
-        right = to_polar((x_right, y))
-
-        return left, right
-    return fn_pass_by, to_polar, to_polar_stereo
-
-
-@app.cell
-def _(
-    c,
-    fn_pass_by,
-    head_radius,
-    mono,
-    np,
-    sample_rate,
-    soundfile,
-    to_polar,
-    to_polar_stereo,
-):
-    time = np.arange(0, len(mono)) / sample_rate
-
-
-    def dopplered_sound(signal, fn_position, origin=(0, 0)):
-        x0, y0 = origin
-        x, y = fn_position(time)
-        r, theta = to_polar((x - x0, y - y0))
-
+    # Calculate the actual time it takes for each sample to reach the listener
+    def dopplered_sound(signal, r):
+        time = np.arange(0, len(signal)) / sample_rate
         sample_reach_time = time - (r / c)
-        dopplered = np.interp(sample_reach_time, time, mono)
+        # Interpolate audio from proper time to sample time
+        dopplered = np.interp(sample_reach_time, time, audio)
 
         return dopplered
 
 
-    def simple_ild(r_left, r_right, power=3):
+    # Calculate L/R gains from distance
+    def simple_ild(r_left, r_right, power=2):
         gain_left = (1 / (r_left + 1e-8)) ** power
         gain_right = (1 / (r_right + 1e-8)) ** power
         return gain_left, gain_right
 
 
-    from scipy.signal import lfilter_zi
-
-
-    dopplered_left = dopplered_sound(mono, fn_pass_by, (-head_radius, 0))
-    dopplered_right = dopplered_sound(mono, fn_pass_by, (head_radius, 0))
-
-    (r_left, theta_left), (r_right, theta_right) = to_polar_stereo(
-        fn_pass_by(time)
-    )
-
-    gain_left, gain_right = simple_ild(r_left, r_right, power=2)
-    dopplered_left *= gain_left
-    dopplered_right *= gain_right
-
-    dopplered = np.vstack((dopplered_left, dopplered_right)).T
-    soundfile.write("out.wav", dopplered, sample_rate)
-    return (gain_left,)
+    # TODO: Apply HRTF
+    return dopplered_sound, fn_elliptic_orbit, simple_ild, to_polar
 
 
 @app.cell
-def _(gain_left, plt):
-    plt.plot(gain_left)
+def _(
+    audio,
+    dopplered_sound,
+    fn_elliptic_orbit,
+    head_radius,
+    mo,
+    np,
+    sample_rate,
+    simple_ild,
+    soundfile,
+    to_polar,
+):
+    # Get source x,y coordinates per sample
+    time = np.arange(0, len(audio)) / sample_rate
+    x, y = fn_elliptic_orbit(time)
+
+    # Convert to polar coordinates centered around left and right ears
+    # TODO: angles `theta` will be used for HRTF (head-related transfer function)
+    # That will filter audio as it passes through the head to reach the ear on the otherside
+    r_left, _ = to_polar((x + head_radius, y))
+    r_right, _ = to_polar((x - head_radius, y))
+
+    # Apply doppler shift in the time domain
+    dopplered_left = dopplered_sound(audio, r_left)
+    dopplered_right = dopplered_sound(audio, r_right)
+
+    # Apply gains according to distance
+    gain_left, gain_right = simple_ild(r_left, r_right)
+    dopplered_left *= gain_left
+    dopplered_right *= gain_right
+
+    # Stack channels into a stereo sound
+    dopplered = np.vstack((dopplered_left, dopplered_right))
+    soundfile.write("out/out.wav", dopplered.T, sample_rate)
+
+    mo.audio(dopplered, rate=sample_rate, normalize=True)
     return
 
 
